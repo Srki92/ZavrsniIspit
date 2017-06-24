6809e13e301f8db49fee535928a6bffcbad8e092
Your Name <you@example.com>
2017-06-24 18:54:45 +0200

    ZavrsniIspit

---
+++ app/src/main/java/com/example/androiddevelopment/agencijazanekretnine/db/ORMLightHelper.java
@@ -0,0 +1,72 @@
+package com.example.androiddevelopment.agencijazanekretnine.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.androiddevelopment.agencijazanekretnine.db.model.Nekretnina;
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+
+/**
+ * Created by androiddevelopment on 24.6.17..
+ */
+
+public class ORMLightHelper extends OrmLiteSqliteOpenHelper {
+
+
+    private static final String DATABASE_NAME = "zavrsni.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private Dao<Nekretnina, Integer> NekretninaDao = null;
+
+
+    public ORMLightHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+
+        try {
+            TableUtils.createTable(connectionSource, Nekretnina.class);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        try {
+            TableUtils.dropTable(connectionSource, Nekretnina.class, true);
+
+            onCreate(database, connectionSource);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Dao<Nekretnina, Integer> getMovieDao() throws SQLException {
+        if (NekretninaDao == null) {
+            NekretninaDao = getDao(Nekretnina.class);
+        }
+
+        return NekretninaDao;
+    }
+
+
+
+    @Override
+    public void close(){
+        NekretninaDao = null;
+
+
+        super.close();
+    }
+
+}
+
---
+++ app/src/main/java/com/example/androiddevelopment/agencijazanekretnine/db/model/Nekretnina.java
@@ -0,0 +1,141 @@
+package com.example.androiddevelopment.agencijazanekretnine.db.model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * Created by androiddevelopment on 24.6.17..
+ */
+@DatabaseTable(tableName = Nekretnina.TABLE_NAME)
+public class Nekretnina  {
+
+    public static final String TABLE_NAME= "nekretnina";
+    public static final String FIELD_NEKRETNINA_ID = "id";
+    public static final String TABLE_NEKRETNINA_NAME = "name";
+    public static final String TABLE_NEKRETNINA_DESCRIPTION = "description";
+    public static final String TABLE_NEKRETNINA_SCORE = "score";
+    public static final String TABLE_NEKRETNINA_ADRESA = "adresa";
+    public static final String TABLE_NEKRETNINA_PICTURES = "movies";
+    public static final String TABLE_NEKRETNINA_PHONE = "phoneNumber";
+    public static final String TABLE_NEKRETNINA_KVADRATURA = "kvadratura";
+    public static final String TABLE_NEKRETNINA_ROOMS ="numberOfRooms";
+
+    @DatabaseField(columnName = FIELD_NEKRETNINA_ID, generatedId = true)
+    private int nId;
+
+    @DatabaseField(columnName = TABLE_NEKRETNINA_NAME)
+    private String nName;
+
+    @DatabaseField(columnName = TABLE_NEKRETNINA_DESCRIPTION)
+    private String nDescription;
+
+    @DatabaseField(columnName = TABLE_NEKRETNINA_SCORE)
+    private float nScore;
+
+    @DatabaseField(columnName = TABLE_NEKRETNINA_ADRESA)
+    private String nAdresa;
+
+    @DatabaseField(columnName = TABLE_NEKRETNINA_PICTURES)
+    private String nPictures;
+
+
+    @DatabaseField(columnName = TABLE_NEKRETNINA_PHONE)
+    private String nPhoneNumber;
+
+
+    @DatabaseField(columnName = TABLE_NEKRETNINA_KVADRATURA)
+    private double nKvadratura;
+
+
+    @DatabaseField(columnName = TABLE_NEKRETNINA_ROOMS)
+    private double nNumberOfRooms;
+
+
+    public Nekretnina() {
+    }
+
+    public Nekretnina(int nId, String nName, String nDescription, float nScore, String nAdresa, String nPictures, String nPhoneNumber, double nKvadratura, double nNumberOfRooms) {
+        this.nId = nId;
+        this.nName = nName;
+        this.nDescription = nDescription;
+        this.nScore = nScore;
+        this.nAdresa = nAdresa;
+        this.nPictures = nPictures;
+        this.nPhoneNumber = nPhoneNumber;
+        this.nKvadratura = nKvadratura;
+        this.nNumberOfRooms = nNumberOfRooms;
+    }
+
+    public int getnId() {
+        return nId;
+    }
+
+    public void setnId(int nId) {
+        this.nId = nId;
+    }
+
+    public String getnName() {
+        return nName;
+    }
+
+    public void setnName(String nName) {
+        this.nName = nName;
+    }
+
+    public String getnDescription() {
+        return nDescription;
+    }
+
+    public void setnDescription(String nDescription) {
+        this.nDescription = nDescription;
+    }
+
+    public float getnScore() {
+        return nScore;
+    }
+
+    public void setnScore(float nScore) {
+        this.nScore = nScore;
+    }
+
+    public String getnAdresa() {
+        return nAdresa;
+    }
+
+    public void setnAdresa(String nAdresa) {
+        this.nAdresa = nAdresa;
+    }
+
+    public String getnPictures() {
+        return nPictures;
+    }
+
+    public void setnPictures(String nPictures) {
+        this.nPictures = nPictures;
+    }
+
+    public String getnPhoneNumber() {
+        return nPhoneNumber;
+    }
+
+    public void setnPhoneNumber(String nPhoneNumber) {
+        this.nPhoneNumber = nPhoneNumber;
+    }
+
+    public double getnKvadratura() {
+        return nKvadratura;
+    }
+
+    public void setnKvadratura(double nKvadratura) {
+        this.nKvadratura = nKvadratura;
+    }
+
+
+    public double getnNumberOfRooms() {
+        return nNumberOfRooms;
+    }
+
+    public void setnNumberOfRooms(double nNumberOfRooms) {
+        this.nNumberOfRooms = nNumberOfRooms;
+    }
+}
